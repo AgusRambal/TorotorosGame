d07fcb2271c21f6fb24e056b893c18f14393de5f
Agustin Rambaldo <agustin.rc97@gmail.com>
2022-02-15 18:36:12 -0300

    Movement of both characters

---
+++ Assets/Scripts/CharacterController2DOtherCharacter.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2DOtherCharacter : MonoBehaviour
+{
+    public float MovementSpeed;
+    public float JumpForce;
+
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var movement = Input.GetAxis("Horixontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (!Mathf.Approximately(0, movement))
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+
+        if (Input.GetButtonDown("Jummp") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+
+    }
+}
